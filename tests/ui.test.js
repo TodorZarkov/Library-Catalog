f6c45da5176d832faf0c3816475c326b88718046
TodorZarkov <t.p.zarkov@gmail.com>
2024-06-11 18:55:12 +0300

    Verify MyBooksLink is visible after login test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -43,3 +43,15 @@ test('Verify "AllBooks" link is visible after user login', async ({page}) =>{
 
     expect(isAllBooksLinkVisible).toBe(true);
 });
+
+test('Verify "My Books" Link Is Visible after user login', async ({page}) => {
+    await page.goto(host + "/login");
+    await page.fill('input[name="email"]', auth1.email);
+    await page.fill('input[name="password"]', auth1.pass);
+    await page.click('input[type="submit"]');
+
+    const myBooksLink = await page.$('a[href="/profile"]');
+    const isMyBooksLinkVisible = await myBooksLink.isVisible();
+
+    expect(isMyBooksLinkVisible).toBe(true);
+} );
