74ca4ee5b7eb0a0fb942eeaa883149afee1d90bc
TodorZarkov <t.p.zarkov@gmail.com>
2024-06-11 18:42:16 +0300

    Verify AllBook Link is visible after login test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,5 +1,9 @@
 const { test, expect } = require('@playwright/test');
 const host = 'http://localhost:3000';
+const auth1 = {
+    email: "peter@abv.bg",
+    pass: "123456"
+};
 
 test('Verify "All Books" link is visible', async ({page}) => {
     await page.goto(host);
@@ -26,4 +30,16 @@ test('Verify "Register" button is visible', async ({page}) => {
     const isRegisterButtonVisible = await registerButton.isVisible();
 
     expect(isRegisterButtonVisible).toBe(true);
+});
+
+test('Verify "AllBooks" link is visible after user login', async ({page}) =>{
+    await page.goto(host + "/login");
+    await page.fill('input[name="email"]', auth1.email);
+    await page.fill('input[name="password"]', auth1.pass);
+    await page.click('input[type="submit"]');
+
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isAllBooksLinkVisible = await allBooksLink.isVisible();
+
+    expect(isAllBooksLinkVisible).toBe(true);
 });
