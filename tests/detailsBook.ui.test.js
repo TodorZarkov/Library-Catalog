9892cd53b901a88f3b155bbd4c518a09bbcf4523
TodorZarkov <t.p.zarkov@gmail.com>
2024-06-16 16:31:22 +0300

    Add test: Verify That All Info Is Displayed Correctly

--- tests/detailsBook.ui.test.js
+++ tests/detailsBook.ui.test.js
@@ -58,5 +58,34 @@ test ("Verify Guest User Sees Details Button and Button Works Correctly", async 
     await addBooksByUser(page, booksOfPeter);
 });
 
+test("Verify That All Details Info Is Displayed Correctly", async ({page}) => {
+    await authenticate(page, authPeter);
+    await deleteAllBooksByUser(page);
 
+    await authenticate(page, authJohn);
+    await deleteAllBooksByUser(page);
+
+    await addBooksByUser(page, [correctBookData]);
+  
+    await navigateToDetailsOfFirstBook(page);
+
+    const detailsPageTitle = await page.textContent('.book-information h3');
+    expect(detailsPageTitle).toBe(correctBookData.title);
+
+    const detailsPageType = await page.textContent('.book-information .type');
+    expect(detailsPageType).toBe('Type: Fiction');
+
+    await expect(page.locator('.book-information .img img'))
+    .toHaveAttribute('src', correctBookData.imageUrl)
+    
+    const detailsPageDetails = 
+    await page.textContent('.book-description p');
+    expect(detailsPageDetails).toBe(correctBookData.description);
+
+    //Back to previous db state:
+
+    await addBooksByUser(page, booksOfJohn);
+    await authenticate(page, authPeter);
+    await addBooksByUser(page, booksOfPeter);
+});
 
