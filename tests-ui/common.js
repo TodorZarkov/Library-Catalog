bda7964c7c8e74f999ace4373589a5e631229e5d
TodorZarkov <t.p.zarkov@gmail.com>
2024-06-26 22:58:08 +0300

    Added job in workflow - test ui

--- tests-ui/common.js
+++ tests-ui/common.js
@@ -1,7 +1,10 @@
 const {test, expect} = require("@playwright/test")
 
 
-export const host = 'http://localhost:3000';
+export let host = 'http://localhost:3000';
+if(process.env.TESTING_HOST){
+    host = process.env.TESTING_HOST;
+}
 
 export const authValid = {
     email: "peter@abv.bg",
